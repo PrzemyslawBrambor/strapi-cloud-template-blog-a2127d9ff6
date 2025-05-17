a709676a03b141365dd2644a46b0867119e856e4
Przemyslaw Brambor <przemyslaw.brambor@tsh.io>
2025-05-17 16:46:46 +0200

    improvements

---
+++ src/api/call-to-action-list-item/controllers/call-to-action-list-item.ts
@@ -0,0 +1,7 @@
+/**
+ * call-to-action-list-item controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::call-to-action-list-item.call-to-action-list-item');
---
+++ src/api/call-to-action-list-item/routes/call-to-action-list-item.ts
@@ -0,0 +1,7 @@
+/**
+ * call-to-action-list-item router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::call-to-action-list-item.call-to-action-list-item');
